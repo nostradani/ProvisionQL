f57f307ec151bd0ed42163c89a017009354f46ce
Daniel Muhra <daniel.muhra@caperwhite.com>
2024-10-28 13:40:10 +0100

    Other

---
+++ ProvionsQLThumbnail/ThumbnailProvider.h
@@ -0,0 +1,13 @@
+//
+//  ThumbnailProvider.h
+//  ProvionsQLThumbnail
+//
+//  Created by Daniel Muhra on 27.10.24.
+//  Copyright © 2024 Evgeny Aleksandrov. All rights reserved.
+//
+
+#import <QuickLookThumbnailing/QuickLookThumbnailing.h>
+
+@interface ThumbnailProvider : QLThumbnailProvider
+
+@end
